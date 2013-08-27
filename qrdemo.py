bf53afb022a994f2fa930b7017539f1ea7f747bc
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2013-08-27 15:13:16 -0400

    create info and instruction pages

--- qrdemo.py
+++ qrdemo.py
@@ -1,16 +1,20 @@
 from bottle import Bottle, run, route, request, response, static_file, template, redirect
 import datetime
 import util
+import qrcode
+import pdb
+import urllib
 
 app = Bottle()
 
 @app.route('/')
 def index():
-	mbox = request.cookies.account
+	mbox = request.cookies.account	
 	if mbox:
 		return template('''you have returned as {{mbox}}<br/>
 			<a href="/signout">sign out</a><br/>
-			scan a "code" <a href="/info/vcr">A code</a>''', mbox=mbox)
+			scan a "code" <a href="/info/vcr">A code</a><br/>
+			<a href="/makeqr">create a qr code<a/>''', mbox=mbox)
 	else:
 		return template('''
 				<p> you are not signed in </p>
@@ -20,6 +24,58 @@ def index():
 				   <input value="Register" type="submit" />
 				</form>
 			''')
+
+@app.route('/info/<partname>')
+def get_info(partname):
+	return template(partname + '_info')
+
+@app.route('/instructions/<partname>')
+def get_instructions(partname):
+	return template(partname + '_instructions')
+
+@app.route('/makeqr')
+def form_qr():
+	return template('''
+			<p>Enter data for QR code</p>
+			<form action="/makeqr" method="post">
+			   name: <input name="name" type="text" />
+			   <br/>
+			   info: <textarea cols="40" rows="5" name="info"></textarea>
+			   <br/>
+			   instructions: <textarea cols="40" rows="5" name="instructions"></textarea>
+			   <br/>			   
+			   <input value="Create" type="submit" />
+			</form><br/><a href='/'>Home</a>
+		''')
+
+@app.route('/makeqr', method='POST')
+def create_qr():
+	name = request.forms.get('name')
+	url_name = urllib.quote_plus(name)
+	instructions = request.forms.get('instructions')
+	info = request.forms.get('info')
+
+	qrname = url_name + '.png'
+	qrdata = 'http://localhost:8099/info/' + url_name
+	img = qrcode.make(qrdata)
+	
+	with open('static/%s' % qrname, 'w+') as qr:
+		img.save(qr, 'PNG')
+
+	info_template_name = url_name + '_info.tpl'
+	with open('views/%s' % info_template_name, 'w+') as tpl:
+		tpl.write("<ul><li>%s</li><li>%s</li><li><a href='/instructions/%s'>instructions</a></li><li><a href='/static/%s'>qrcode</a></li></ul>" % (name, info, url_name, qrname))
+
+	instruction_template_name = url_name + '_instructions.tpl'
+	with open('views/%s' % instruction_template_name, 'w+') as tpl:
+		tpl.write("<ul><li>%s</li><li>%s</li><li><a href='/info/%s'>info</a></li></ul>" % (name, instructions, url_name))
+
+	# return redirect('/static/' + qrname)
+	return redirect('/info/' + url_name)
+
+@app.route('/static/<filename>')
+def server_static(filename):
+	return static_file(filename, root='./static')
 
 @app.route('/register', method='POST')
 def do_reg():
@@ -47,11 +103,5 @@ def signout():
 	if acc_cookie:
 		response.set_cookie('account', '', expires=datetime.datetime.now())
 	redirect('/') 
-
-@app.route('/<action>/<item>')
-def item_api(action='list', item='all'):
-	# return items[action][item]
-	mbox = request.cookies.get('account', 'unknown')
-	return template('Action was {{action}}<br/>Item was {{item}}<br/>Mbox was {{mbox}}', action=action, item=item, mbox=mbox)
 
 run(app, server='gunicorn', host='localhost', port=8099, debug=True, reloader=True)
